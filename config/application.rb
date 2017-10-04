3278e43e05f7de2d4e6d69e41129bdf49c9c90b2
Stuart Marquis <marquis@columbia.edu>
2017-10-04 15:52:34 -0400

    VALET-41 - Gracefully handle fatal errors

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,9 @@ module Valet
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # https://mattbrictson.com/dynamic-rails-error-pages
+    config.exceptions_app = self.routes
+    
   end
 end
