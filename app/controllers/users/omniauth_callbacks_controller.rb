d2873fb18bfa66a73deaf746efd9d9d8df434963
Stuart Marquis <stuartmarquis@gmail.com>
2017-11-07 11:22:29 -0500

    Fix for an exception we saw in prod

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   include Cul::Omniauth::Callbacks
 
+  def new_session_path(scope)
+    new_user_session_path # this accomodates Users namespace of the controller
+  end
+
   def affiliations(user, affils)
     return unless user
     user.affils = affils.sort
