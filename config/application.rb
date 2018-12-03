edc5f11d408e561b928a66ac3cf2c689859190f8
Stuart Marquis <stuartmarquis@gmail.com>
2018-12-03 14:26:04 -0500

    Improved /admin/system debuting

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+VALET_VERSION = IO.read('VERSION').strip
+
 module Valet
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
