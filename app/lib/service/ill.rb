94d2e200e85b5afa82fd8bd9af3456662560d06c
Stuart Marquis <stuartmarquis@gmail.com>
2020-12-02 15:19:44 -0500

    code cleanup

--- app/lib/service/ill.rb
+++ app/lib/service/ill.rb
@@ -1,25 +1,27 @@
-module Service
-  class Ill < Service::Base
-
-    def form_handler(params, bib_record, _current_user)
-      openurl = bib_record.openurl
-      ezproxy_url = @config[:ezproxy_url]
-
-      campus = params['campus']
-      illiad_url = build_illiad_url(campus)
-
-      redirect_url = ezproxy_url +
-                     '?url=' + illiad_url +
-                     '?' + openurl
-      redirect_to redirect_url
-    end
-
-    # TODO: - placeholder logic, campus string label != CGI path component
-    def build_illiad_url(campus)
-      illiad_url = 'https://columbia.illiad.oclc.org/illiad/' +
-                   campus + '/illiad.dll/OpenURL'
-      illiad_url
-    end
-
-  end
-end
+# 
+# This service was never completed.
+# 
+# module Service
+#   class Ill < Service::Base
+#
+#     def form_handler(params, bib_record, _current_user)
+#       openurl = bib_record.openurl
+#
+#       campus = params['campus']
+#       illiad_url = build_illiad_url(campus)
+#
+#       redirect_url = ezproxy_url +
+#                      '?url=' + illiad_url +
+#                      '?' + openurl
+#       redirect_to redirect_url
+#     end
+#
+#     # TODO: - placeholder logic, campus string label != CGI path component
+#     def build_illiad_url(campus)
+#       illiad_url = 'https://columbia.illiad.oclc.org/illiad/' +
+#                    campus + '/illiad.dll/OpenURL'
+#       illiad_url
+#     end
+#
+#   end
+# end
